0eabb65df9dc2e0ac7ea31871b863edf70ab6c5e
wayneblink <darylblink@ymail.com>
2023-08-27 12:07:35 -0400

    moving to lazy nvim

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,40 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+y")
+
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader><leader>", "<C-^>")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-p>", "<cmd>Telescope git_files<cr>")
+vim.keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>ps", "<cmd>Telescope grep<cr>")
+vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<cr>")
+vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>")
+vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>")
+
+vim.keymap.set("n", "<M-CR>", ":lua vim.lsp.buf.code_action()<cr>")
+
+vim.keymap.set("n", "<leader>xx", function() require("trouble").open() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").open("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").open("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").open("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end)
+
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
--- nvim/lua/wayneblink/init.lua
+++
@@ -1,2 +0,0 @@
-require("wayneblink.remap")
-require("wayneblink.set")
