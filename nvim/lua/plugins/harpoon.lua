76dc8591a78fd432e4c4fd6ac3517d142660e4b1
dblink_navsea <daryl.w.blink.civ@us.navy.mil>
2023-10-31 15:08:22 -0400

    update tmux theme

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -4,7 +4,9 @@ return {
     local mark = require "harpoon.mark"
     local ui = require "harpoon.ui"
 
-    require("harpoon").setup()
+    require("harpoon").setup({
+      tabline = true,
+    })
 
     vim.keymap.set("n", "<leader>a", mark.add_file)
     vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
@@ -13,6 +15,5 @@ return {
     vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
     vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
     vim.keymap.set("n", "<C-h>", function() ui.nav_file(4) end)
-
   end,
 }
