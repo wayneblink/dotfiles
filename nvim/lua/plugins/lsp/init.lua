b989117aea80f6e57fb6a1b2eb461f1a9219b114
wayneblink <darylblink@ymail.com>
2024-02-09 19:44:50 -0500

    update noice

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -112,8 +112,6 @@ return {
     config = function()
       require("mason-tool-installer").setup({
         ensure_installed = {
-          "autopep8",
-          "flake8",
           "js-debug-adapter",
           "prettierd",
           "stylua",
