6e48d7ab59057b2d2166d746b8bb1a3ab1f31ec0
wayneblink <darylblink@ymail.com>
2024-02-10 07:15:07 -0500

    move trouble keymaps

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -10,6 +10,21 @@ return {
       vim.keymap.set("n", "<leader>tt", function()
         require("trouble").toggle()
       end)
+      vim.keymap.set("n", "<leader>tw", function()
+        require("trouble").open("workspace_diagnostics")
+      end)
+      vim.keymap.set("n", "<leader>td", function()
+        require("trouble").open("document_diagnostics")
+      end)
+      vim.keymap.set("n", "<leader>tq", function()
+        require("trouble").open("quickfix")
+      end)
+      vim.keymap.set("n", "<leader>tl", function()
+        require("trouble").open("loclist")
+      end)
+      vim.keymap.set("n", "gR", function()
+        require("trouble").open("lsp_references")
+      end)
     end
   },
 }
