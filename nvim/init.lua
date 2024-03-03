a823dc2f8f5e0dc11bbd48a5b0aa95b10348e0cf
wayneblink <darylblink@ymail.com>
2024-03-03 18:12:25 -0500

    add git signs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,11 @@
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
