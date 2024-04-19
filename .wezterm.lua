23e3855cb62d3966a960f84988c759d557722679
wayneblink <darylblink@ymail.com>
2024-04-19 18:57:30 -0400

    disable wezterm full screen toggle

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,4 +4,8 @@ local config = wezterm.config_builder()
 config.enable_tab_bar = false
 config.font = wezterm.font('Berkeley Mono')
 
+config.keys = {
+  { key = 'Enter', mods = 'ALT', action = wezterm.action.DisableDefaultAssignment },
+}
+
 return config
