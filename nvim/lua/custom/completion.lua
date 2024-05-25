9e8bb6ac8fea720ee5bc91380f5cd4c168b8f399
wayneblink <darylblink@ymail.com>
2024-05-24 23:27:55 -0400

    remove dadbod

--- nvim/lua/custom/completion.lua
+++ nvim/lua/custom/completion.lua
@@ -33,11 +33,3 @@ cmp.setup {
     end,
   },
 }
-
--- Setup up vim-dadbod
-cmp.setup.filetype({ "sql" }, {
-  sources = {
-    { name = "vim-dadbod-completion" },
-    { name = "buffer" },
-  },
-})
--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -7,7 +7,6 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-telescope/telescope-smart-history.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
-    "kkharji/sqlite.lua",
   },
   config = function()
     require "custom.telescope"
