ea66392bc6120e365b8f8e5f4188f3da9350a0a5
Wayne Blink <darylblink@ymail.com>
2025-02-12 15:24:57 -0500

    show hidden files in oil

--- nvim/lua/config/plugins/oil.lua
+++ nvim/lua/config/plugins/oil.lua
@@ -5,5 +5,12 @@ return {
     --@type oil.SetupOpts
     opts = {},
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    config = function()
+      require("oil").setup({
+        view_options = {
+          show_hidden = true
+        }
+      })
+    end,
   }
 }
