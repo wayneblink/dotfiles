1012c71892904dc1145534ea9d833251172c47ad
wayneblink <darylblink@ymail.com>
2026-08-21 22:28:15 -0500

    update wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,15 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrains Mono")
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-config.color_scheme = "tokyonight_night"
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.warn_about_missing_glyphs = false
 config.enable_scroll_bar = true
 config.window_decorations = "RESIZE"
